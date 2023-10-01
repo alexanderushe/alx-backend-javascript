11213e3a5e13c771e8293456793a74b3236f6920
alexanderushe <alex.f.ushe@gmail.com>
2023-10-01 12:59:36 +0200

    building sqft

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+/**
+ * implement a building class
+ */
+
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'class extending building must override evacuationWarningMessage',
+        );
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
