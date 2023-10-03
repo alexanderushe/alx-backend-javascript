34f22bfa4c7b9105b9047216b9717b321741487f
alexanderushe <alex.f.ushe@gmail.com>
2023-10-03 18:05:47 +0200

    get list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const students = [
+    { id: 1, name: 'Guillaume', location: 'San Fransisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
+}
