1379699d1afd8d2052e2fe6f5c555ad0d70262fd
alexanderushe <alex.f.ushe@gmail.com>
2023-10-04 14:50:51 +0200

    get list.

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,10 @@
 function getListStudents() {
-     const students =  [
-    { id: 1, name: 'Guillaume', location: 'San Fransisco' },
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
   return students;
 }
+
 export default getListStudents;
