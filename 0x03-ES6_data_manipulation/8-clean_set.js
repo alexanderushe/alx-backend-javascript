c1bd830022efaae69626649f25a99c82a33725f0
alexanderushe <alex.f.ushe@gmail.com>
2023-10-05 11:16:13 +0200

    grocery list

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,15 @@
-function cleanSet(set, startString){
-  const filteredValues = Array.from(set)
-  .filter((value) => value.startsWith(startString))
-  .map((value) => value.slice(startString.length));
-  return filteredValues.join('-');
-}
-export default cleanSet;
+function cleanSet(set, startString) {
+    // Filter the set to only include values that start with the startString.
+    const filteredSet = Array.from(set)
+    .filter((value) => value.startsWith(startString));
+  
+    // Map the filtered set to remove the startString from each value.
+    const mappedSet = filteredSet.map((value) => value.substring(startString.length));
+  
+    // Join the mapped set values with a hyphen.
+    const joinedSet = mappedSet.join('-');
+  
+    // Return the joined set string.
+    return joinedSet;
+  }
+  export default cleanSet;
