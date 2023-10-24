6a5c47998d37333d562c9c169d635840c6f4ee8c
Alex Ushe <alex.f.ushe@gmail.com>
2023-10-24 03:42:45 +0200

    corrected lines
    white space correction

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,11 @@
-export default function createIteratorObject(report){
-  return (function* _() {
-    for (const department of Object.values(report.allEmployees)) {
-      for (const employee of department){
-        yield employee;
+export default function createIteratorObject(report) {
+    return (function* _() {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const department of Object.values(report.allEmployees)) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const employee of department) {
+          yield employee;
+        }
       }
-    }
-  }());
-}
+    }());
+  }
