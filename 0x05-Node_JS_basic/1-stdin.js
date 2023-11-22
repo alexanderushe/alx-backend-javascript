a59d4bbae6f9ccf86bc3de0c41e95a498e0170f2
alexanderushe <alex.f.ushe@gmail.com>
2023-11-22 08:27:48 +0200

    stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,9 @@
-const readline = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-console.log('welcome to Holberton school, what is your name?');
-
-r1.on('line', (line) => {
-  console.log(`Your name is: ${line}`);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is ${data}`);
 });
 
-r1.on('close', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing.\n');
 });
